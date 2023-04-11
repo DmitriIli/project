a0ed1a231b351116b13344d61d8513dd5720f9f9
dili <di.grebenev@outlook.com>
2023-04-11 14:40:39 +0300

    commit

--- silant/frontend/src/components/App.js
+++ silant/frontend/src/components/App.js
@@ -1,41 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { render } from "react-dom";
-import Search from './UI/Search';
+import React from "react";
 
 
-function App() {
-
-  const [dataJoined, setDataJoined] = useState('')
-  const [error, setError] = useState()
-
-  const [value, setValue] = useState()
-
-  useEffect(() => {
-    fetch('api/index/')
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        } else {
-          throw Error(`Something went wrong: code ${response.status}`)
-        }
-      })
-      .then(({ data }) => {
-        setDataJoined(data)
-      })
-      .catch(error => {
-        console.log(error)
-        setError('Ошибка, подробности в консоли')
-      })
-  }, [])
-
-  return (
-    <div className="App">
-      <Search />
-    </div>
-  );
-
+export default function App() {
+    return (
+        <div>App</div>
+    )
 }
-export default App;
 
-const container = document.getElementById("app");
-render(<App />, container);
+ 
