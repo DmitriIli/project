89072683534c17eee35de84d1e2e25dc5365206a
dili <di.grebenev@outlook.com>
2023-06-07 16:58:30 +0300

    add

--- silant/client/src/components/TableRow.jsx
+++ silant/client/src/components/TableRow.jsx
@@ -1,19 +1,17 @@
 import React, { useState } from 'react';
 import Cell from './Cell';
 
-// let keys = Object.keys(i)
 
 const TableRow = (data) => {
     return (
-        <div className="table-row">
-            {
-                // data.map(item =>
-                //     <Cell data={item} key={item} />
-                // )
-                Object.keys(data).map = (keyItem) => {
-                    <Cell data={data[keyItem]} key={data[keyItem] + keyItem} />
-                }
-            }
+        <div >
+            {Object.keys(data.data).map((item)=>{
+                return(
+                    <>
+                        {console.log(item)}
+                    </>
+                )
+            })}
         </div>
     )
 };
